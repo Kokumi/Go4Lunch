1efd666bc7e8e93d398e11afc8d3aa726a603380
Debruyckere <fldebruyck@numericable.fr>
2019-03-01 14:28:35 +0100

    add Google map

--- app/src/main/java/com/debruyckere/florian/go4lunch/Controller/Activity/appActivity.java
+++ app/src/main/java/com/debruyckere/florian/go4lunch/Controller/Activity/appActivity.java
@@ -1,16 +1,13 @@
 package com.debruyckere.florian.go4lunch.Controller.Activity;
 
-import android.support.annotation.NonNull;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 import com.debruyckere.florian.go4lunch.Model.ViewPagerAdapter;
 import com.debruyckere.florian.go4lunch.R;
 
-public class appActivity extends AppCompatActivity {
+public class appActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/debruyckere/florian/go4lunch/Controller/Fragment/RestaurantMapFragment.java
+++ app/src/main/java/com/debruyckere/florian/go4lunch/Controller/Fragment/RestaurantMapFragment.java
@@ -1,17 +1,28 @@
 package com.debruyckere.florian.go4lunch.Controller.Fragment;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.debruyckere.florian.go4lunch.R;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class RestaurantMapFragment extends BaseFragment {
+
+public class RestaurantMapFragment extends BaseFragment implements OnMapReadyCallback {
+
+    private MapView mMapView;
+    private GoogleMap mMap;
+
+
 
     public RestaurantMapFragment() {
         // Required empty public constructor
@@ -24,6 +35,7 @@ public class RestaurantMapFragment extends BaseFragment {
      */
     public static RestaurantMapFragment newInstance() {
         RestaurantMapFragment fragment = new RestaurantMapFragment();
+
         return fragment;
     }
 
@@ -35,7 +47,31 @@ public class RestaurantMapFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_restaurant_map, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_restaurant_map, container, false);
+
+        mMapView = view.findViewById(R.id.fragment_mapView);
+        mMapView.onCreate(savedInstanceState);
+        mMapView.onResume();
+
+        try{
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        mMapView.getMapAsync(this);
+
+        return view;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+
+        LatLng point = new LatLng(37,151);
+        mMap.addMarker(new MarkerOptions().position(point).title("Marker in Nowhere"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(point));
     }
 }
